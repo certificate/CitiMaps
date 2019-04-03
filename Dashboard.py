e07ac06a79149b9d23a56cbf9882258b0db9376d
Valtteri Vuori <valtteri.vuori@live.fi>
2019-04-03 18:18:15 +0300

    Combined the two files to work together

--- Dashboard.py
+++ Dashboard.py
@@ -1,20 +1,53 @@
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, show, output_file
-from bokeh.tile_providers import CARTODBPOSITRON
+from bokeh.tile_providers import CARTODBPOSITRON_RETINA
 
-output_file("tile.html")
+import csvReader
 
-# range bounds supplied in web mercator coordinates
-p = figure(x_range=(-2000000, 6000000), y_range=(-1000000, 7000000),
-           x_axis_type="mercator", y_axis_type="mercator")
 
-source = ColumnDataSource(
-    data=dict(lat=[ 30.29,  30.20,  30.29],
-              lon=[-97.70, -97.74, -97.78])
-)
+def stations_to_lists(stations):
+    lat = []
+    lon = []
 
-p.circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, source=source)
+    for station in stations:
+        lat.append(station.latitude)
+        lon.append(station.longitue)
+        print("Did one!")
 
-p.add_tile(CARTODBPOSITRON)
+    return lat, lon
 
-show(p)
+
+def get_data():
+    csvReader.import_data()
+
+
+def main():
+    print("Running main function")
+    output_file("tile.html")
+
+    get_data()
+
+    stations = csvReader.stationList
+
+    # range bounds supplied in web mercator coordinates
+    p = figure(x_range=(-2000000, 6000000), y_range=(-1000000, 7000000),
+               x_axis_type="mercator", y_axis_type="mercator")
+
+    latlist, lonlist = stations_to_lists(stations)
+
+    source = ColumnDataSource(
+        data=dict(lat=latlist,
+                  lon=lonlist)
+    )
+
+    p.circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, source=source)
+
+    p.add_tile(CARTODBPOSITRON_RETINA)
+
+    show(p)
+
+
+if __name__ == '__main__':
+    main()
+
+
--- csvReader.py
+++ csvReader.py
@@ -1,7 +1,6 @@
 import csv
 import time
 
-
 class Station:
     def __init__(self, name, lat, lon, stationId):
         self.name = name
@@ -10,48 +9,50 @@ class Station:
         self.stationId = stationId
 
 
-# Array for all the different variables which we will be using later.
+def import_data():
+
+    # Array for all the different variables which we will be using later.
+
+
+    # Time the task just to see how efficient it is.
+    t0 = time.time()
+
+    # Let's open the behemoth of a file! (Close to 5 million lines of sweet, sweet data!) Remember to check if your
+    # file has been named differently. Filename underneath was the one I got from unpacking the zip file.
+    with open("citibike.csv", 'r', encoding='latin-1') as data:
+        reader = csv.reader(data, delimiter=",")
+        print("Working...")
+
+        # Skipping the first row. Doesn't containt anything but the explanations.
+        for _ in range(0, 1):
+            next(data)
+
+        # Go through the entire file line by line, doing subtasks whenever encountering a
+        # certain type of a vehicle.
+
+        skipList = []
+        for line in reader:
+
+            # type of the vehicle
+            lat = line[5]
+            lon = line[6]
+            stationId = line[3]
+            name = line[4]
+
+            if (lat, lon, stationId, name) is not None:
+
+                if stationId not in skipList:
+                    stationList.append(Station(name, lat, lon, stationId))
+                    skipList.append(stationId)
+
+
+        # End calculations for time. My personal record on my home rig was 27 seconds (i7-6700K @ 4.4GHz)
+
+        t1 = time.time()
+        total = t1 - t0
+        print("The whole operation took {} seconds.".format(round(total, 2)))
+
+        print(len(stationList))
+
 stationList = []
-
-# Time the task just to see how efficient it is.
-t0 = time.time()
-
-# Let's open the behemoth of a file! (Close to 5 million lines of sweet, sweet data!) Remember to check if your
-# file has been named differently. Filename underneath was the one I got from unpacking the zip file.
-with open("citibike.csv", 'r', encoding='latin-1') as data:
-    reader = csv.reader(data, delimiter=",")
-    print("Working...")
-
-    # Skipping the first row. Doesn't containt anything but the explanations.
-    for _ in range(0, 1):
-        next(data)
-
-    # Go through the entire file line by line, doing subtasks whenever encountering a
-    # certain type of a vehicle.
-
-    skipList = []
-    for line in reader:
-
-        # type of the vehicle
-        lat = line[5]
-        lon = line[6]
-        stationId = line[3]
-        name = line[4]
-
-        if (lat, lon, stationId, name) is not None:
-
-            if stationId not in skipList:
-                stationList.append(Station(name, lat, lon, stationId))
-                skipList.append(stationId)
-
-
-    # End calculations for time. My personal record on my home rig was 27 seconds (i7-6700K @ 4.4GHz)
-
-    t1 = time.time()
-    total = t1 - t0
-    print("The whole operation took {} seconds.".format(round(total, 2)))
-
-    print(len(stationList))
-
-
 
